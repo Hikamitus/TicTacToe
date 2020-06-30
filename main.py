105443d0026c91446316ea2cc6a7ab034f1c88fb
Hikami <hikamitsukami@gmail.com>
2020-06-29 21:19:29 -0300

    Correção de um MicroBug

--- main.py
+++ main.py
@@ -67,7 +67,6 @@ class TicTacToe:
                         break
                     else:
                         print('O local já foi escolhido!');
-                        local = int(input('Local: '));
                 except:
                     continue
         
